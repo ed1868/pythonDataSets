381f35911bb0fb3f52710c4ee30ce02a832ddfda
eddieruiz <eddi@overseasinternational.com>
2020-02-16 10:10:11 -0500

    printing headers

--- panda.py
+++ panda.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 path = "https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/auto.csv"
 df = pd.read_csv(path, header=None)
+
+print("The first 5 rows of the dataframe") 
+df.head(5)
